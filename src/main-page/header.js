63d415476443dbe04f6d43fa7137fedcb906a3b8
Your Name <you@example.com>
2023-08-09 15:42:23 +0100

    logo added

---
+++ src/main-page/header.js
@@ -0,0 +1,14 @@
+import logo from './Globomantics.png';
+
+const Header = () => (
+    <Header className="row">
+        <div className="col-md-5">
+            <img src={logo} className="logo" alt="logo" />
+        </div>
+
+
+
+
+
+    </Header>
+);
